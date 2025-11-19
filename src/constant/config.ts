72c0d5855cb65d673441dce76b660f19f1c0c1e6
Rio R <40910296+rio-rsa@users.noreply.github.com>
2025-11-19 19:48:57 +0800

    chore: initial commit

---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: "Rio's Personal Website",
+  description:
+    'Welcome to my personal website, where I share my work, projects, and ideas.',
+  url: 'https://riothinks.com',
+};
